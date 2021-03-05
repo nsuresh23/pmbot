2822bdb7aecc6ea7374fb1081910d41b3e1fe595
suresh <N.sureshkumar@spi-global.com>
2021-03-05 15:19:45 +0530

    [UPDATE]: Email Escalation feature

--- resources/views/pages/dashboard/qc/dashboardQCEmails.blade.php
+++ resources/views/pages/dashboard/qc/dashboardQCEmails.blade.php
@@ -6,7 +6,7 @@
         <aside class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
             <div class="inbox-body email-list-body pa-0">
                 <div id="qcEmail" class="myEmailGrid emailGrid tab-pane fade pt-0 active in" data-category="qcEmail"
-                    data-type="dashboard" data-email-filter="" data-list-url="{{ $myEmailListUrl }}"
+                    data-type="dashboard" data-email-filter="qc_email" data-list-url="{{ $myEmailListUrl }}"
                     data-current-route="{{ $currentRoute }}" data-current-user-id="{{ Auth::user()->empcode }}"></div>
             </div>
             @include('pages.email.qcEmailDetail')
